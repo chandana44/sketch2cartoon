3f84161f46d35eebf3f006cb4d3ea3fd18ce8460
Pandian <pandian4mail@gmail.com>
2017-11-25 11:50:42 -0600

    Changes to sketchify to accept input and output directory as arguments

--- sketchify.py
+++ sketchify.py
@@ -4,14 +4,14 @@ from scipy.ndimage import filters
 from skimage import io
 import glob, os
 
-in_dir = 'train'
-out_dir = 'sketches'
-if not os.path.exists(out_dir): os.mkdir(out_dir)
 
-def main():
-    for files1 in glob.glob(in_dir + '/*.jpg'):
+def generate_sketches(in_dir, out_dir):
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    for files1 in glob.glob(in_dir + '/*.png'):
         filepath, filename = os.path.split(files1)
-        Gamma = 0.97
+        Gamma = 0.98
         Phi = 200
         Epsilon = 0.1
         k = 2
@@ -20,7 +20,7 @@ def main():
         im = Image.open(files1).convert('L')
         im = array(ImageEnhance.Sharpness(im).enhance(3.0))
         im2 = filters.gaussian_filter(im, Sigma)
-        im3 = filters.gaussian_filter(im, Sigma* k)
+        im3 = filters.gaussian_filter(im, Sigma * k)
         differencedIm2 = im2 - (Gamma * im3)
         (x, y) = shape(im2)
         for i in range(x):
@@ -32,5 +32,10 @@ def main():
 
         io.imsave(os.path.join(out_dir, filename), differencedIm2.astype(np.uint8))
 
+
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 3:
+        print('Please provide <input_photos_directory> and <output_sketches_directory> as arguments..')
+        sys.exit(1)
+
+    generate_sketches(sys.argv[1], sys.argv[2])
