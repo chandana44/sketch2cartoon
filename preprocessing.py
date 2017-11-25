d98c23636604caedd2604b48fc924d957eb812b9
Pandian <pandian4mail@gmail.com>
2017-11-25 11:55:59 -0600

    Modifying preprocessing to reflect the latest changes, adding input/output directory as arguments

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,24 @@
+from __future__ import print_function
 from os import listdir
 from os.path import isfile, join
-from PIL import Image
-from resizeimage import resizeimage
+import sys
 
-raw_images_path = "minions/"
-cropped_images_path = "cartoons/"
+from PIL import Image
+
+
+if len(sys.argv) != 3:
+    print('Please provide <raw_images_path> and <cropped_images_path> as arguments..')
+    sys.exit(1)
+
+raw_images_path = sys.argv[1]
+cropped_images_path = sys.argv[2]
 
 raw_images_paths = [f for f in listdir(raw_images_path) if isfile(join(raw_images_path, f))]
 for image_path in raw_images_paths:
-    print image_path
+    print(image_path)
     with open(raw_images_path + image_path, 'r+b') as f:
         with Image.open(f) as image:
-            cover = resizeimage.resize_cover(image, [256, 256])
-            cover.save(cropped_images_path + image_path, image.format)
-
+            image = image.resize((256, 256), Image.ANTIALIAS)
+            image.save(cropped_images_path + image_path, image.format)
+            #cover = resizeimage.resize_cover(image, [256, 256])
+            #cover.save(cropped_images_path + image_path, image.format)
